a5fb2a1b0feafe2b628fcd43a6a43b38a3467b2e
lucasfrosty <lucasfr@cc.ci.ufpb.br>
2017-05-25 08:02:05 -0300

    map area finished

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-function initMap() {
+// TODO: put the button inside the map
+// TODO: comment the code
+//
 
+function initMap() {
   let brasilia = {lat: -15.7801, lng: -47.9292};
   let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: brasilia
   });
-
   $.getJSON('places.json', (data) => {
     let allPlaces = [];
     $.each(data, function(key, val) {
@@ -15,9 +17,32 @@ function initMap() {
     allPlaces.forEach((element) => {
       new google.maps.Marker({
         position: {lat: element.lat, lng: element.lng},
-        map: map
+        map: map,
+        icon: '../../images/icons/marker-pink.png'
       });
     });
   });
 
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(latitude, longitude),
+                    map: map,
+                    title: "Current Location",
+                    icon: "http://i.imgur.com/S1ooXKy.png"
+                });
+
+    map.setCenter(new google.maps.LatLng(latitude, longitude));
+    map.setZoom(7);
+  }
+
+  $( "#find-me-btn" ).click(function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  });
 }
